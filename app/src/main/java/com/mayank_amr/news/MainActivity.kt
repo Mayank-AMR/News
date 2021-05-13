f0ab6173aed5a13b29ceef6587515332b13de5cf
Mayank Kumar <m1y1nk.amr@gmail.com>
2021-05-13 12:52:45 +0530

    Project Initialised with m minSdkVersion 19 and targetSdkVersion 30

---
+++ app/src/main/java/com/mayank_amr/news/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.mayank_amr.news
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
